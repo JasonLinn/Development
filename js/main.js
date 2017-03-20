c7caf6e6a8b981038c5aceee94ca825727cfeed9
sexfat <bryant6831@gmail.com>
2017-03-20 12:25:03 +0800

    update

--- js/main.js
+++ js/main.js
@@ -4,23 +4,19 @@ $('.banner_carousel').owlCarousel({
     animateOut: 'fadeOut',
     loop:true,
     nav:true,
-<<<<<<< HEAD
     items: 1,
     
 });
 console.log('ok1');
-=======
+
     items: 1
 })
-
 
 //menu effect
 
 $(document).ready(function(){
 
-   
-
 
 });
 
->>>>>>> feature/mainpage
+
